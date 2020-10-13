8ea103e31e2ee09c119704f4afde246b60955a4e
Nick <mornind@gmail.com>
2020-10-13 11:10:24 +0300

    Some minor improvements

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type celsius float64
-type kelvin float64
-type farenheit float64
 
 func (c celsius) toKelvin() kelvin {
 	return kelvin(c + 273.15)
@@ -14,6 +16,8 @@ func (c celsius) toFarenheit() farenheit {
 	return farenheit((c * 9.0 / 5.0) + 32.0)
 }
 
+type kelvin float64
+
 func (k kelvin) toCelsius() celsius {
 	return celsius(k - 273.15)
 }
@@ -21,6 +25,8 @@ func (k kelvin) toCelsius() celsius {
 func (k kelvin) toFarenheit() farenheit {
 	return farenheit((k-273.15)*9.0/5.0 + 32.0)
 }
+
+type farenheit float64
 
 func (f farenheit) toCelsius() celsius {
 	return celsius((f - 32.0) * 5.0 / 9.0)
@@ -30,7 +36,31 @@ func (f farenheit) toKelvin() kelvin {
 	return kelvin((f-32.0)*5.0/9.0 + 273.15)
 }
 
+type sensor func() kelvin
+
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
+}
+
+func realSensor() kelvin {
+	return 0 // TODO: Внедрить реальный сенсор
+}
+
+func measureTemperature(samples int, sensor sensor) {
+	for i := 0; i < samples; i++ {
+		k := sensor()
+		fmt.Printf("%v° K\n", k)
+		time.Sleep(time.Second)
+	}
+}
+
+func calibrate(s sensor, offset kelvin) sensor {
+	return func() kelvin {
+		return s() + offset
+	}
+}
+
 func main() {
-	var t celsius = 127
-	fmt.Println(t.toFarenheit())
+	sensor := fakeSensor
+	measureTemperature(5, sensor)
 }
