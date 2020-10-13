67f6cec20ea08175ff477e9ae86d838f33f1553c
Nick <mornind@gmail.com>
2020-10-13 10:41:19 +0300

    reworked functions to methods

--- main.go
+++ main.go
@@ -6,27 +6,31 @@ type celsius float64
 type kelvin float64
 type farenheit float64
 
-// kelvinToCelcius конвертирует Кельвины в градусы Цельсия
-func kelvinToCelsius(k kelvin) celsius {
-	return celsius(k - 273.15)
+func (c celsius) toKelvin() kelvin {
+	return kelvin(c + 273.15)
 }
 
-// celsiusToFarenheit ковертирует градусы Цельсия в градусы Фаренгейта
-func celsiusToFarenheit(c celsius) farenheit {
+func (c celsius) toFarenheit() farenheit {
 	return farenheit((c * 9.0 / 5.0) + 32.0)
 }
 
-// kelvinToFarenheit конвертирует Кельвины в градусы Фаренгейта
-func kelvinToFarenheit(k kelvin) farenheit {
+func (k kelvin) toCelsius() celsius {
+	return celsius(k - 273.15)
+}
+
+func (k kelvin) toFarenheit() farenheit {
 	return farenheit((k-273.15)*9.0/5.0 + 32.0)
 }
 
-// celsiusToKelvin конвертирует градусы Цельсия в Кельвины
-func celsiusToKelvin(c celsius) kelvin {
-	return kelvin(c + 273.15)
+func (f farenheit) toCelsius() celsius {
+	return celsius((f - 32.0) * 5.0 / 9.0)
+}
+
+func (f farenheit) toKelvin() kelvin {
+	return kelvin((f-32.0)*5.0/9.0 + 273.15)
 }
 
 func main() {
 	var t celsius = 127
-	fmt.Println(celsiusToKelvin(t))
+	fmt.Println(t.toFarenheit())
 }
